b2cc38ea69bbccc925167b98cfb18ec9dfd2ee76
antonelidanielerobles-a11y <antonelidanielerobles@gmail.com>
2026-08-23 16:17:59 -0300

    Update MainActivity.java

--- app/src/main/java/com/roblesantoneli/vancontrolpro/MainActivity.java
+++ app/src/main/java/com/roblesantoneli/vancontrolpro/MainActivity.java
@@ -2,6 +2,8 @@ package com.roblesantoneli.vancontrolpro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,37 +12,168 @@ public class MainActivity extends Activity {
 
     private WebView webView;
 
+    private static final String URL_VANCONTROL =
+        "https://script.google.com/macros/s/AKfycbxM7vqTgNJwv5eyoFITjGy-vd16PKCYZtQfT1nqaSTszFsia8CYb5InRwEu0IMBjwC4/exec";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
 
-        WebSettings webSettings = webView.getSettings();
+        webView =
+            new WebView(this);
 
+
+        WebSettings webSettings =
+            webView.getSettings();
+
+
+        /*
+         * =====================================
+         * JAVASCRIPT
+         * =====================================
+         */
         webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(
+            true
+        );
+
+
+        /*
+         * =====================================
+         * ARMAZENAMENTO
+         * =====================================
+         */
         webSettings.setDomStorageEnabled(true);
+
+        webSettings.setDatabaseEnabled(true);
+
+
+        /*
+         * =====================================
+         * RESPONSIVIDADE
+         * =====================================
+         */
         webSettings.setLoadWithOverviewMode(true);
+
         webSettings.setUseWideViewPort(true);
 
+
+        /*
+         * =====================================
+         * CACHE
+         * =====================================
+         *
+         * Ajuda a deixar as próximas
+         * aberturas mais rápidas.
+         */
+        webSettings.setCacheMode(
+            WebSettings.LOAD_DEFAULT
+        );
+
+
+        /*
+         * =====================================
+         * COOKIES
+         * =====================================
+         *
+         * Muito importante para
+         * Google Apps Script.
+         */
+        CookieManager cookieManager =
+            CookieManager.getInstance();
+
+        cookieManager.setAcceptCookie(
+            true
+        );
+
+        CookieManager.setAcceptFileSchemeCookies(
+            true
+        );
+
+        cookieManager.setAcceptThirdPartyCookies(
+            webView,
+            true
+        );
+
+
+        /*
+         * =====================================
+         * WEBVIEW CLIENT
+         * =====================================
+         *
+         * Mantém a navegação dentro
+         * do aplicativo.
+         */
         webView.setWebViewClient(
             new WebViewClient()
         );
 
-        setContentView(webView);
 
-webView.loadUrl(
-    "https://script.google.com/macros/s/AKfycbxM7vqTgNJwv5eyoFITjGy-vd16PKCYZtQfT1nqaSTszFsia8CYb5InRwEu0IMBjwC4/exec"
-);
+        /*
+         * =====================================
+         * WEB CHROME CLIENT
+         * =====================================
+         *
+         * Necessário para recursos
+         * mais completos de JavaScript.
+         */
+        webView.setWebChromeClient(
+            new WebChromeClient()
+        );
+
+
+        /*
+         * =====================================
+         * MOSTRA O WEBVIEW
+         * =====================================
+         */
+        setContentView(
+            webView
+        );
+
+
+        /*
+         * =====================================
+         * ABRE O VANCONTROL
+         * =====================================
+         */
+        webView.loadUrl(
+            URL_VANCONTROL
+        );
     }
+
 
     @Override
     public void onBackPressed() {
 
-        if (webView.canGoBack()) {
+        if (
+            webView != null &&
+            webView.canGoBack()
+        ) {
+
             webView.goBack();
+
         } else {
+
             super.onBackPressed();
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        if (webView != null) {
+
+            webView.destroy();
+
+            webView =
+                null;
+        }
+
+        super.onDestroy();
     }
 }
